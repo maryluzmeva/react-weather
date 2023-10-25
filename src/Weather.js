d3bbba031a4fa5a6749239b2dcd5feed6d1533eb
maryluzmeva <maryluzmeva@gmail.com>
2023-10-25 18:49:06 +0200

    APi update

--- src/Weather.js
+++ src/Weather.js
@@ -32,8 +32,8 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const apiKey = "c71f439f65td859373faeeba102o0222";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,10 +17,10 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let apiKey = "c71f439f65td859373faeeba102o0222";
+    let lon = props.coordinates.lon;
+    let lat= props.coordinates.lat;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?lon={longitude}&lat={latitude}&key={apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
   }
