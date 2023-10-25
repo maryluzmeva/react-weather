bb1a55d2c1da2c6e3824a5516d68b0e32eef3463
maryluzmeva <maryluzmeva@gmail.com>
2023-10-25 16:26:27 +0200

    Create the basic Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-      Created by 👩🏽‍💻
-      <a href= "https://www.linkedin.com/in/maryluz-mendez-vargas-bbb23824/"
-      target="_blank"
-      rel="noreferrer">
-        Maryluz Mendez Vargas
-      </a> {" "}
-      and is Open-sourced on {" "}
-      <a href="https://github.com/maryluzmeva/react-weather"
-      target="_blank"
-      rel="noreferrer"> 
-       GitHub </a>
-      </footer>
+      <div className="container">
+        <Weather defaultCity="Berlin" />
+
+        
+        <footer>
+          Created by 👩🏽‍💻
+          <a href= "https://www.linkedin.com/in/maryluz-mendez-vargas-bbb23824/"
+          target="_blank"
+          rel="noreferrer">
+          Maryluz Mendez Vargas
+          </a> {" "}
+          and is Open-sourced on {" "}
+          <a href="https://github.com/maryluzmeva/react-weather"
+          target="_blank"
+          rel="noreferrer"> 
+          GitHub </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-reportWebVitals();
