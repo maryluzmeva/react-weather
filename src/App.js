d78fe5a005df7f2f637ea3f01bd5ed21e30ba5c2
maryluzmeva <maryluzmeva@gmail.com>
2023-10-25 16:05:15 +0200

    Create the Footer for Author info on HTML File

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+      Created by 👩🏽‍💻
+      <a href= "https://www.linkedin.com/in/maryluz-mendez-vargas-bbb23824/"
+      target="_blank"
+      rel="noreferrer">
+        Maryluz Mendez Vargas
+      </a> {" "}
+      and is Open-sourced on {" "}
+      <a href="https://github.com/maryluzmeva/react-weather"
+      target="_blank"
+      rel="noreferrer"> 
+       GitHub </a>
+      </footer>
     </div>
   );
 }
