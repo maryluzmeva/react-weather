fac46736cd00946283ee0eb07857c960556fd66f
maryluzmeva <maryluzmeva@gmail.com>
2023-10-26 12:54:38 +0200

    Changing Background CSS

--- src/Weather.js
+++ src/Weather.js
@@ -9,6 +9,7 @@ export default function Weather(props) {
   const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
+    console.log (response.data);
     setWeatherData({
       ready: true,
       coordinates: response.data.coordinates,
@@ -16,7 +17,7 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       date: new Date(response.data.time * 1000),
       description: response.data.condition.description,
-      icon: response.data.condition.icon,
+      icon: response.data.icon,
       wind: response.data.wind.speed,
       city: response.data.city,
     });
