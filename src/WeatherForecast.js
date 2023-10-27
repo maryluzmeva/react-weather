e7dee8bb72fe5794c40fa42747939026a6a59887
maryluzmeva <maryluzmeva@gmail.com>
2023-10-27 17:38:45 +0200

    WeatherForecast and ForecastDay update

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
@@ -7,27 +7,45 @@ export default function WeatherForecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
 
+    useEffect(() => {
+        setLoaded(false);
+      }, [props.coordinates]);
+
     function handleResponse(response) {
         setForecast(response.data.daily);
         setLoaded(true);
     }
-    if (loaded) {
-        return (
-            <div className="row WeatherForecast">
-                <div className="col-2">
-                    <WeatherForecastDay data={forecast[0]} />
-                </div>
-            </div>
-        );
-    } else {
 
+    function load () {
         let apiKey = "a1b283feoeccefb140t55b69080a1da6";
         let lon = props.coordinates.longitude;
         let lat = props.coordinates.latitude;
         let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
 
         axios.get(apiUrl).then(handleResponse);
-
-        return null;
     }
-}; 
+
+    if (loaded) {
+        return (
+          <div className="WeatherForecast">
+            <div className="row">
+              {forecast.map (function(dailyForecast, index) {
+                if (index < 5) {
+                  return (
+                    <div className="col" key={index}>
+                      <WeatherForecastDay data={dailyForecast} />
+                    </div>
+                  );
+                } else {
+                    return null;
+                  }
+                })}
+              </div>
+            </div>
+          );
+        } else {
+          load();
+      
+          return null;
+        }
+      }
